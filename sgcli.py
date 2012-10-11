96e60be8324322707d883c73b526f9f7bde578bd
Mike Dirolf <mike@dirolf.com>
2012-10-11 15:51:59 -0400

    Working on search

--- sgcli.py
+++ sgcli.py
@@ -1,5 +1,10 @@
 import curses
-from curses.ascii import (ESC)
+from curses.ascii import (isalnum,
+                          BS,
+                          DEL,
+                          ESC,
+                          SP)
+import curses.textpad
 import os
 
 
@@ -16,8 +21,10 @@ WIDTH = 80
 
 
 def main(stdscr):
+    stdscr.keypad(0)
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+
     draw_home(stdscr)
 
     while True:
@@ -31,14 +38,39 @@ def main(stdscr):
             draw_search(stdscr)
 
 
+def centered(win, y, message, *args):
+    win.addstr(y, (WIDTH - len(message)) / 2, message, *args)
+
+
 def draw_search(stdscr):
     stdscr.clear()
-    stdscr.addstr(2, 10, "SEARCH:")
+    stdscr.border()
+    centered(stdscr, 2, "Enter a performer, event or venue:")
+    curses.curs_set(2)
+
+    input = ""
+
+    while True:
+        # Search bar
+        stdscr.addstr(4, WIDTH / 2 - 20, "_" * 40, curses.A_DIM);
+        stdscr.addstr(4, WIDTH / 2 - 20, input);
+
+        ev = stdscr.getch()
+        if ev == SP:
+            input += " "
+        if ev in (BS, DEL):
+            input = input[:-1]
+        elif ev == ESC:
+            return draw_home(stdscr)
+        elif isalnum(ev):
+            input += chr(ev)
+
     stdscr.refresh()
 
 
 def draw_home(stdscr, message=None):
     stdscr.clear()
+    stdscr.border()
 
     x = (WIDTH - 50) / 2
     stdscr.addstr(2, x, " ;;;;               ;   ", curses.color_pair(1))
@@ -67,8 +99,14 @@ def draw_home(stdscr, message=None):
     stdscr.addstr(14, x + 24, "        `;,      .;:      ")
     stdscr.addstr(15, x + 24, "          ,;;;;;;;        ")
 
-    if message:
-        stdscr.addstr(20, (WIDTH - len(message)) / 2, message)
+    message_2 = None
+    if not message:
+        message = "Welcome to SeatGeek!"
+        message_2 = "(h) home  (s) search  (q/ESC) quit"
+
+    centered(stdscr, 19, message)
+    if message_2:
+        centered(stdscr, 20, message_2)
 
     stdscr.refresh()
 
