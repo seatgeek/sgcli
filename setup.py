600390e7fae972d4f4a74f7cc5dc915fb9a1ea26
Mike Dirolf <mike@dirolf.com>
2012-10-12 15:18:56 -0400

    Trying to package this thing.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SG',
+      version='0.1',
+      description='SeatGeek command line app',
+      author='Mike Dirolf',
+      author_email='mike@dirolf.com',
+      url='http://github.com/mdirolf/sgcli',
+      scripts=['sgcli'],
+     )
